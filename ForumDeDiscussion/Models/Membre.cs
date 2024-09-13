90288f56525575be035dce0b483935a054d54dbf
2434546 <2434546@etu.cchic.ca>
2024-09-12 23:15:37 -0400

    qdqd

--- ForumDeDiscussion/Models/Membre.cs
+++ ForumDeDiscussion/Models/Membre.cs
@@ -2,12 +2,17 @@
 {
     public class Membre
     {
+        public const string ROLE_ADMIN = "Admin";
+        public const string ROLE_STANDARD = "Standard";
+        public const string ROLE_INVITE = "Invité";
+        
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Firstname { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string UserName { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Firstname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Role { get; set; } = ROLE_INVITE;
         public List<Message> Messages { get; set; }
     }
 }
