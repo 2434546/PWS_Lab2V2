af03f86aa5cd8f244771bfc3be480145f48403a6
2434546 <2434546@etu.cchic.ca>
2024-09-22 13:15:31 -0400

    qdqsdsqd

--- ForumDeDiscussion/Models/Section.cs
+++ ForumDeDiscussion/Models/Section.cs
@@ -5,8 +5,7 @@ namespace ForumDeDiscussion.Models
     public class Section
     {
         public int Id { get; set; }
-
-        [Display(Name = "Nom de la section")]
+        
         public string Title { get; set; } = string.Empty;
 
         public List<Subject> Subjects { get; set; } = new List<Subject>();
